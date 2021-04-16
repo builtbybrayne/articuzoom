de60eac69e69f4c1aa138de84321f57847e51063
Alastair Brayne <al@builtbybrayne.com>
2021-04-16 23:12:32 +0100

    Table display tweaks

--- src/components/Scores.tsx
+++ src/components/Scores.tsx
@@ -4,6 +4,7 @@ import {Column} from "primereact/column";
 import {DataTable} from "primereact/datatable";
 import {Dict} from "../types";
 import setProp from 'lodash.set';
+import getProp from 'lodash.get';
 
 export function RoundScores({suggestions}: { suggestions: Suggestion[] }) {
     const data = useMemo(() => {
@@ -37,7 +38,9 @@ export function GameScores({game}: { game: GameType }) {
                     const {winner} = suggestion;
                     if (winner) {
                         const count = data[winner]?.[round] || 0;
+                        const total = data[winner]?.total || 0;
                         setProp(data, [winner, round], count + 1);
+                        setProp(data, [winner, 'total'], total + 1);
                     }
                 })
             });
@@ -50,10 +53,18 @@ export function GameScores({game}: { game: GameType }) {
         });
     }, [scores]);
 
+    console.log('DATA', data);
+
     return <div className="p-shadow-1">
-        <DataTable value={data}>
-            <Column field="name" header="Player"/>
+        <DataTable
+            value={data}
+            autoLayout
+            scrollable
+            className='p-datatable-striped'
+        >
+            <Column field="name" header="Player" style={{fontWeight: 'bold'}}/>
             {scores ? Object.keys(scores).map(round => <Column key={round} field={round} header={`Round ${round}`}/>) : undefined}
+            <Column field="total" header="Total" style={{fontWeight: 'bold'}}/>
         </DataTable>
     </div>
 }
